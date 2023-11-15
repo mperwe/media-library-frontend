4aa455f0559c84a0fa37975c9ec29a842d9650e9
Mperwe <frankmperwe@gmail.com>
2023-11-15 12:22:34 +0100

    generate forms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Axios from "axios";
 import styled from "styled-components";
-import MovieComponent from "./components/MovieComponent";
-import MovieInfoComponent from "./components/MovieInfoComponent";
 
 export const API_KEY = "a9118a3a";
 
@@ -10,11 +8,13 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
 const AppName = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
 `;
+
 const Header = styled.div`
   background-color: black;
   color: white;
@@ -27,6 +27,7 @@ const Header = styled.div`
   font-weight: bold;
   box-shadow: 0 3px 6px 0 #555;
 `;
+
 const SearchBox = styled.div`
   display: flex;
   flex-direction: row;
@@ -42,6 +43,7 @@ const MovieImage = styled.img`
   height: 48px;
   margin: 15px;
 `;
+
 const SearchInput = styled.input`
   color: black;
   font-size: 16px;
@@ -50,82 +52,135 @@ const SearchInput = styled.input`
   outline: none;
   margin-left: 15px;
 `;
-const MovieListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  padding: 1px;
-  gap: 25px;
-  justify-content: space-evenly;;
+
+const RegisterButton = styled.button`
+  background-color: #2ecc71;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
-const Placeholder = styled.img`
-  width: 800px;
-  height: 400px;
-  margin: 150px;
-  opacity: 100%;
-  
+
+const RegisterFormContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-end;  // Align to the right
+justify-content: center;
+height: 100vh;
+padding: 20px;
+background-image: url('/moviebackground.jpg');
+background-size: cover;
+background-position: center;
+justify-content: flex-start;
+`;
+
+const RegisterForm = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-end;  // Align to the right
+
+`
+const RegisterFormInput = styled.input`
+  margin-bottom: 10px;
+  padding: 15px;
+  width: 88%;
+`;
+
+const BackgroundCard = styled.div`
+  width: 38%;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: white;  // Set the background color to white
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);  // Optional: Add a box shadow for a card-like appearance
+  height: 30vh;
 `;
 
 function App() {
   const [searchQuery, updateSearchQuery] = useState("");
-
+  // eslint-disable-next-line
   const [movieList, updateMovieList] = useState([]);
+  // eslint-disable-next-line
   const [selectedMovie, onMovieSelect] = useState();
 
-  const [timeoutId, updateTimeoutId] = useState();
+  // New state for registration form
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
+  // New state for showing/hiding registration form
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
 
   const fetchData = async (searchString) => {
-    const response = await Axios.get(
-      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
-    );
+    const backendUrl = "http://localhost:4100/movies/";
+    const response = await Axios.get(backendUrl);
     updateMovieList(response.data.Search);
   };
 
   const onTextChange = (e) => {
-    onMovieSelect("")
-    clearTimeout(timeoutId);
+    onMovieSelect("");
     updateSearchQuery(e.target.value);
-    const timeout = setTimeout(() => fetchData(e.target.value), 500);
-    updateTimeoutId(timeout);
+    fetchData(e.target.value);
   };
+
+  const handleRegisterFormSubmit = (e) => {
+    e.preventDefault();
+    // Add your user registration logic here
+    // For simplicity, we'll just log the registration details
+    console.log("User registered:", {
+      username: registerUsername,
+      email: registerEmail,
+      password: registerPassword,
+    });
+    // Close the registration form
+    setShowRegisterForm(false);
+  };
+
   return (
     <Container>
       <Header>
         <AppName>
           <MovieImage src="logo12.jpg" />
           Frank's Movie Room
-         
         </AppName>
         <SearchBox>
-         
           <SearchInput
             placeholder="Search Movie"
             value={searchQuery}
             onChange={onTextChange}
           />
         </SearchBox>
+        <RegisterButton onClick={() => setShowRegisterForm(true)}>
+          Login
+        </RegisterButton>
       </Header>
-      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect} />}
-      <MovieListContainer>
-        {movieList?.length ? (
-          movieList.map((movie, index) => (
-            <MovieComponent
-              key={index}
-              movie={movie}
-              onMovieSelect={onMovieSelect}
-            />
-          ))
-        ) : (
-          <Placeholder src="moviebg.jpg" />
-        )}
 
-          <MovieImage src="logo12.jpg" />
 
-          
-        
-      </MovieListContainer>
+      <RegisterFormContainer>
+  <RegisterForm >
+  <BackgroundCard>
+
+    <RegisterFormInput
+      type="text"
+      placeholder="Username"
+    />
+    <RegisterFormInput
+      type="email"
+      placeholder="Email"
+    />
+    <RegisterFormInput
+      type="password"
+      placeholder="Password"
+    />
+    <RegisterButton type="submit">Register</RegisterButton>
+    </BackgroundCard>
+
+  </RegisterForm>
+</RegisterFormContainer>
+
     </Container>
   );
+
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,58 @@
+// LoginForm.js
+
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const LoginFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const LoginFormInput = styled.input`
+  margin-bottom: 10px;
+  padding: 8px;
+`;
+
+const LoginButton = styled.button`
+  background-color: #3498db;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Add your login logic here
+    console.log("User logged in:", { username, password });
+  };
+
+  return (
+    <LoginFormContainer>
+      <form>
+        <LoginFormInput
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <LoginFormInput
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <LoginButton onClick={handleLogin}>Login</LoginButton>
+      </form>
+    </LoginFormContainer>
+  );
+};
+
+export default LoginForm;
