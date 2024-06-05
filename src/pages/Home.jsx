0d27af8bc84087f2ca93e0dd43f9ab150a8fd831
mperwe <frankmperwe@gmail.com>
2024-06-05 11:13:48 +0200

    edit home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,82 +1,83 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'; // Import Link from React Router
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import 'react-toastify/dist/ReactToastify.css';
+
+// Background image
+import backgroundImage from '../images/moviebg11.jpg';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+`;
+
+const Background = styled.div`
+  flex: 1;
+  background-image: url(${backgroundImage});
   background-size: cover;
+  background-position: center;
 `;
 
 const Content = styled.div`
-  flex: 1;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  padding: 30px 0;
+  flex: 1;
 `;
 
 const CardContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 85px; 
+  width: 90%;
+  max-width: 600px;
 `;
 
 const Card = styled(Link)`
-  position: relative;
-  width: 2000px; /* Adjusted width */
-  height: 700px; /* Adjusted height */
+  width: 100%;
   background-color: #fff;
   border: 1px solid #ccc;
-  margin: 10px;
+  margin-bottom: 20px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   text-decoration: none;
   overflow: hidden;
 `;
 
 const CardImage = styled.img`
-  width: 80%; /* Adjusted width */
-  height: 95%; /* Adjusted height */
+  width: 100%;
+  height: auto;
   object-fit: cover;
 `;
 
 const CardContent = styled.div`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
   text-align: center;
-  padding: 20px;
-  color: #fff; /* Adjusted text color */
+  padding: 10px;
+  color: #000;
 `;
 
 const Title = styled.h2`
   margin: 5px;
   font-family: 'Arial', sans-serif;
-  font-size: 50px;
+  font-size: 18px;
   font-weight: bold;
 `;
 
 const ContextText = styled.p`
   margin: 5px 0 0;
   font-family: 'Arial', sans-serif;
-  font-size: 30px;
+  font-size: 14px;
 `;
 
 const Button = styled.button`
-  margin-top: 20px;
-  padding: 30px 40px;
-  font-size: 18px;
+  margin-top: 10px;
+  padding: 5px 10px;
+  font-size: 14px;
   background-color: green;
   color: white;
   border: none;
-  border-radius: 30px;
+  border-radius: 20px;
   cursor: pointer;
   font-weight: bold;
 `;
@@ -84,19 +85,22 @@ const Button = styled.button`
 const Home = () => {
   return (
     <Container>
-      <Header />
-      <Content>
-        <CardContainer>
-          <Card to={`/page1`}>
-            <CardImage src={`movie24.jpeg`} alt={`Image 1`} />
-            <CardContent>
-              <Title>Where Every Frame Tells a Theme Story</Title>
-              <ContextText>'Unraveling Mysteries, One Movie at a Time!'</ContextText>
-              <Link to={`/View`}><Button>Explore More with Us</Button></Link>
-            </CardContent>
-          </Card>
-        </CardContainer>
-      </Content>
+      <Background>
+        <Header />
+        <Content>
+          <CardContainer>
+            <Card to={`/page1`}>
+              <CardImage src={`movie24.jpeg`} alt={`Image 1`} />
+              <CardContent>
+                <Title>Where Every Frame Tells a Theme Story</Title>
+                <ContextText>'Unraveling Mysteries, One Movie at a Time!'</ContextText>
+                <Link to={`/View`}><Button>Explore More with Us</Button></Link>
+              </CardContent>
+            </Card>
+            {/* Add more cards here if needed */}
+          </CardContainer>
+        </Content>
+      </Background>
       <Footer />
     </Container>
   );
